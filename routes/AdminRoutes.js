4986eba318a4e96f5e990b06d1c7ed92e2d0f298
deadman962011 <deadman962011@gmail.com>
2019-12-30 13:41:05 -0800

    new-Routes

---
+++ routes/AdminRoutes.js
@@ -0,0 +1,56 @@
+var express = require('express');
+
+var router = express.Router();
+
+
+router.get('/Catigory',function (req,res,next) {
+    
+})
+
+router.post('/Catigory',function (req,res,next) {
+    
+})
+
+
+
+
+router.get('/AddBlog',function (req,res,next) {
+    
+})
+
+router.post('/AddBlog',function (req,res,next) {
+    
+})
+
+
+router.get('/DelBlog',function (req,res,next) {
+    
+})
+
+
+
+router.post('/DelBlog',function (req,res,next) {
+    
+})
+
+
+router.get('/UpdateBlog',function (req,res,next) {
+    
+})
+
+
+router.get('/UpdateBlog/:BlogId',function(req,res,next){
+
+})
+
+
+router.post('/UodateBlog/:BlogId',function(req,res,next){
+
+})
+
+
+
+
+
+
+module.exports = router;
